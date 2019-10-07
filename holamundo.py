5b5f8bb2bc299c43fcca51f0b3d71702522d65c0
José A <jagalindo@us.es>
2019-10-07 11:28:52 +0200

    Update holamundo.py
    This allows the execution under docker

--- holamundo.py
+++ holamundo.py
@@ -20,6 +20,6 @@ def hello_name(name):
 if __name__ == '__main__':      
 # run() method of Flask class runs the application     
 # on the local development server.     
-    app.run()
+    app.run(port=80, debug=True, host='0.0.0.0')
 
 #credits to https://www.geeksforgeeks.org/flask-creating-first-simple-application/
